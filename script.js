0c16d3117fe7c84c7853b9c6ceaa87c2fabdded4
SMasilela <mt.sgp.management@gmail.com>
2024-08-22 12:03:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-    import { io } from 'socket.io-client'
+
+document.addEventListener('DOMContentLoaded', function() {
+    const io = require('socket.io-client')
+    
 
     const socket = io('http://localhost:3000')
-    
     const buyButton = document.getElementById('buyButton');
     const sellButton = document.getElementById('sellButton');
     const closeAllButton = document.getElementById('closeAllButton');
@@ -20,50 +22,13 @@
 
     buyButton.addEventListener('click', function() {
         const inputValue = numericInput.value;
-        /*if (!isNaN(inputValue)) {
-            fetch(BASE_URL+"?type=cwrite&command="+inputValue+"_buy")
-			.then(res=>res.json())
-            .then(pos=>{
-                console.log(pos);
-                result=pos;
-            })
-			.then(data=>{
-				data.forEach(dt => {
-					elemnt.insertAdjacentHTML("beforeend","<option value='"+dt.login+"'>"+dt.login+"</option>");
-				});
-            updateResult(result);
-        }*/
     });
 
     sellButton.addEventListener('click', function() {
         const inputValue = numericInput.value;
-        /*if (!isNaN(inputValue)) {
-            fetch(BASE_URL+"?type=cwrite&command="+inputValue+"_sell","content-type:application/json")
-			.then(res=>res.json())
-            .finally(pos=>{
-                result=pos;
-            })
-			.then(data=>{
-				data.forEach(dt => {
-					elemnt.insertAdjacentHTML("beforeend","<option value='"+dt.login+"'>"+dt.login+"</option>");
-				});
-            updateResult(result);
-        }*/
     });
 
     closeAllButton.addEventListener('click', function() {
         const inputValue = numericInput.value;
-        /*if (!isNaN(inputValue)) {
-            fetch(BASE_URL+"?type=cwrite&command="+inputValue+"_closeall","content-type:application/json")
-			.then(res=>res.json())
-            .finally(pos=>{
-                result=pos;
-            })
-			.then(data=>{
-				data.forEach(dt => {
-					elemnt.insertAdjacentHTML("beforeend","<option value='"+dt.login+"'>"+dt.login+"</option>");
-				});
-            updateResult(result);
-        }*/
     });
-
+});
